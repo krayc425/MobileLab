39baee193366b00ebc43e8eed8c2e93b0b8c4af0
Kuixi Song <krayc425@gmail.com>
2018-05-15 18:35:36 +0800

    Init ArchitectureDemo

---
+++ ArchitectureDemo/app/src/main/java/cn/edu/nju/candleflame/architecturedemo/dagger/use/MainActivityComponent.java
@@ -0,0 +1,8 @@
+package cn.edu.nju.candleflame.architecturedemo.dagger.use;
+
+import dagger.Component;
+
+@Component
+public interface MainActivityComponent {
+    void inject(DaggerFragment fragment);
+}
---
+++ ArchitectureDemo/app/src/main/java/cn/edu/nju/candleflame/architecturedemo/mvc/bean/NumberBean.java
@@ -0,0 +1,13 @@
+package cn.edu.nju.candleflame.architecturedemo.mvc.bean;
+
+public class NumberBean {
+    private int num = 2018;
+
+    public int getNum() {
+        return num;
+    }
+
+    public void setNum(int num) {
+        this.num = num;
+    }
+}
